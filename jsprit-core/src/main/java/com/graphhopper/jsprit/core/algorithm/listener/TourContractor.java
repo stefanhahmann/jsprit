46e81a84359c4389ead402b93905463170274656
hahmann <stefan.hahmann@ivi.fraunhofer.de>
2021-10-05 17:00:54 +0200

    Add further cases to TourContractor

--- jsprit-core/src/main/java/com/graphhopper/jsprit/core/algorithm/listener/TourContractor.java
+++ jsprit-core/src/main/java/com/graphhopper/jsprit/core/algorithm/listener/TourContractor.java
@@ -2,10 +2,11 @@ package com.graphhopper.jsprit.core.algorithm.listener;
 
 import java.util.Collection;
 
+import org.apache.commons.collections4.iterators.ReverseListIterator;
+
 import com.graphhopper.jsprit.core.problem.VehicleRoutingProblem;
 import com.graphhopper.jsprit.core.problem.solution.VehicleRoutingProblemSolution;
 import com.graphhopper.jsprit.core.problem.solution.route.VehicleRoute;
-import com.graphhopper.jsprit.core.problem.solution.route.activity.Start;
 import com.graphhopper.jsprit.core.problem.solution.route.activity.TourActivity;
 
 public class TourContractor implements AlgorithmEndsListener
@@ -18,21 +19,38 @@ public class TourContractor implements AlgorithmEndsListener
         {
             for (VehicleRoute route : solution.getRoutes())
             {
-                for (TourActivity activity : route.getActivities())
+                ReverseListIterator<TourActivity> reverseActivityIterator = new ReverseListIterator(
+                    route.getActivities());
+
+                // loop backwards through the list of tour activities
+                while (reverseActivityIterator.hasNext())
                 {
+                    TourActivity activity = reverseActivityIterator.next();
                     TourActivity precedingActivity = activity.getPredecessor();
+                    if (precedingActivity == null)
+                        continue;
                     precedingActivity.getEndTime();
                     activity.getTransportTimeFromPredecessor();
-                    // TODO maybe this even makes sense also for cases where the preceding activity is not of type Start
-                    if (precedingActivity != null && precedingActivity instanceof Start
-                        && activity.getTheoreticalEarliestOperationStartTime() != 0
+
+                    // for activities with time windows: contract tour such that the vehicle leaves the preceding activity location only at the time, when it is needed to reach the current activity location at the beginning of the time window
+                    if (activity.getTheoreticalEarliestOperationStartTime() != 0
                         && activity.getTheoreticalEarliestOperationStartTime() - activity
                             .getTransportTimeFromPredecessor() > precedingActivity.getEndTime())
                     {
                         precedingActivity
                             .setEndTime(activity.getTheoreticalEarliestOperationStartTime()
                                 - activity.getTransportTimeFromPredecessor());
+                        precedingActivity.setArrTime(
+                            precedingActivity.getEndTime() - precedingActivity.getOperationTime());
                         activity.setArrTime(activity.getTheoreticalEarliestOperationStartTime());
+                    }
+
+                    // adjust the leaving time at the preceding activity such that the preceding activity location is only left at the time, when it is needed to reach the current activity location
+                    else if (activity.getArrTime() - activity
+                        .getTransportTimeFromPredecessor() > precedingActivity.getEndTime())
+                    {
+                        precedingActivity.setEndTime(
+                            activity.getArrTime() - activity.getTransportTimeFromPredecessor());
                     }
                 }
             }
